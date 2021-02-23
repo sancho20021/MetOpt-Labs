b45ce6cad92cce1ed834723a441921cb8054f3c8
Ahmad <iahmd01@gmail.com>
2021-02-23 16:33:18 +0300

    hello nice to meet you

--- lab1/src/Test.java
+++ lab1/src/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello, Sasha!");
+        System.out.println("Hello, Sasha! Nice to meet you");
     }
 }
