5c491e06cd1b3d499a850882ceacf0153f9e6e61
Yaroslav Ilin <FMyb@github.com>
2021-05-13 23:20:38 +0300

    added matrix reading from file

---
+++ src/lab3/Utils.java
@@ -0,0 +1,31 @@
+package lab3;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * @author Yaroslav Ilin
+ * So I am writing this code, so I am writing input rules.
+ */
+public class Utils {
+    static ProfileFormatMatrix readMatrix(File file) throws FileNotFoundException {
+        Scanner in = new Scanner(file);
+        if (!in.hasNextInt()) {
+            throw new IllegalArgumentException("expected dimension");
+        }
+        int n = in.nextInt();
+        double[][] a = new double[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!in.hasNextInt()) {
+                    throw new IllegalArgumentException("expected matrix values on position (" + i + " " + j + ")");
+                }
+                a[i][j] = in.nextInt();
+            }
+        }
+        return new ProfileFormatMatrix(n, a);
+    }
+}
