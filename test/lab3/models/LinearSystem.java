f2c98cdebec8f7199fa83ea0f62e45226ca4ee6a
Ahmad <iahmd01@gmail.com>
2021-05-19 19:53:37 +0300

    new features for  LinearSystem

--- test/lab3/models/LinearSystem.java
+++ test/lab3/models/LinearSystem.java
@@ -1,16 +1,25 @@
 package lab3.models;
 
+import java.util.Optional;
+
 public class LinearSystem {
     private final FullMatrix matrix;
     private final Vector b;
-    private final Vector correctAnswer;
+    private final Optional<Vector> correctAnswer;
 
     public LinearSystem(final FullMatrix matrix, final Vector b, final Vector correctAnswer) {
         this.matrix = matrix;
         this.b = b;
-        this.correctAnswer = correctAnswer;
+        this.correctAnswer = Optional.of(correctAnswer);
         assert matrix.size() == b.getDim();
         assert matrix.size() == correctAnswer.getDim();
+    }
+
+    public LinearSystem(final FullMatrix matrix, final Vector b) {
+        this.matrix = matrix;
+        this.b = b;
+        this.correctAnswer = Optional.empty();
+        assert matrix.size() == b.getDim();
     }
 
     public FullMatrix getMatrix() {
@@ -22,11 +31,11 @@ public class LinearSystem {
     }
 
     public Vector getCorrectAnswer() {
-        return correctAnswer;
+        return correctAnswer.orElse(null);
     }
 
     public Vector getError(final Vector other) {
-        return correctAnswer.subtract(other);
+        return correctAnswer.orElse(null).subtract(other);
     }
 
     public double getEuclideanError(final Vector other) {
