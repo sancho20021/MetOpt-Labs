577355f28046d862c4f679f1ebde6774235219be
Yaroslav Ilin <FMyb@github.com>
2021-06-04 01:00:45 +0300

    add Abstract QuasiNewtonianMinimizer. (#1) Check CHECK and TODO

--- src/methods/multidimensional/newton/methods/PowellMethod.java
+++ src/methods/multidimensional/newton/methods/PowellMethod.java
@@ -10,55 +10,20 @@ import java.util.stream.DoubleStream;
 /**
  * @author Yaroslav Ilin
  */
-public class PowellMethod extends NewtonMinimizer {
-    private Vector x;
-    private Vector s;
-    private Vector gx;
-    private Vector d;
-    private AdvancedMatrix g;
+public class PowellMethod extends QuasiNewtonianMinimizer {
 
     protected PowellMethod(AnalyticFunction fun, Vector startX, double eps) {
         super(fun, startX, eps);
-        restart();
     }
 
     @Override
-    public boolean hasNext() {
-        return s == null || s.getEuclideanNorm() > eps;
-    }
-
-    @Override
-    protected Vector nextIteration() {
-        // :SORRY: я не научился искать минимум функции
-        double r = 0;
-        // :TODO: r = argmin alpha (f(x + alpha * d))
-        s = d.multiply(r);
-        x = x.add(s);
-        Vector gy = gx;
-        gx = fun.getGradient(x);
-        Vector p = gx.subtract(gy);
-        Vector v = g.multiply(p);
+    public AdvancedMatrix getG(Vector v, Vector p, Vector s) {
         Vector y = s.add(v);
-        g = g.subtract(
+        return g.subtract(
                 y.multiply(y)
                         .multiply(
                                 1 / p.scalarProduct(y)
                         )
-        );
-        d = g.multiply(-1).multiply(gx);
-        return x;
-    }
-
-    @Override
-    public void restart() {
-        x = startX;
-        gx = fun.getGradient(x);
-        d = gx.multiply(-1);
-        g = new DiagonalMatrix(DoubleStream.of(1).limit(startX.size()).toArray());
-    }
-
-    @Override
-    public Vector getCurrentXMin() {
-        return x;
+        ); // :CHECK: Check correct
     }
 }
