c617cac4b097a29efd3e727cae8dd6316a5015a9
Alexander <sancho20021@gmail.com>
2021-06-04 11:39:30 +0300

    Task 1.2

--- test/methods/multidimensional/newton/methods/MinimizationTester.java
+++ test/methods/multidimensional/newton/methods/MinimizationTester.java
@@ -7,20 +7,35 @@ import org.junit.Assert;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MinimizationTester {
     private final AnalyticMinimizer.AnalyticMinimizerCons minimizerConstructor;
     private final List<MinimizationTask> tasks = new ArrayList<>(List.of(
-            new MinimizationTask(new AnalyticFunction(1, "x_{0}^2"), new Vector(0), new Vector(10)),
-            Task12.f1,
-            Task12.f2
+            new MinimizationTask(new AnalyticFunction(1, "x_{0}^2"), new Vector(0), new Vector(10))
     ));
     public final double eps;
     public final static double STANDARD_EPS = 1e-7;
+    public final boolean showIterationsNumber;
+    public final boolean showPoints;
+    public final String methodName;
 
-    public MinimizationTester(final AnalyticMinimizer.AnalyticMinimizerCons minimizerConstructor, final double eps) {
+    public MinimizationTester(
+            final AnalyticMinimizer.AnalyticMinimizerCons minimizerConstructor,
+            final double eps,
+            final boolean showIterationsNumber,
+            final boolean showPoints,
+            final String methodName
+    ) {
         this.minimizerConstructor = minimizerConstructor;
         this.eps = eps;
+        this.showIterationsNumber = showIterationsNumber;
+        this.showPoints = showPoints;
+        this.methodName = methodName;
+    }
+
+    public MinimizationTester(final AnalyticMinimizer.AnalyticMinimizerCons minimizerConstructor, final double eps) {
+        this(minimizerConstructor, eps, true, false, "Unnamed method");
     }
 
     public MinimizationTester(final AnalyticMinimizer.AnalyticMinimizerCons minimizerConstructor) {
@@ -45,15 +60,27 @@ public class MinimizationTester {
     }
 
     public void testAll(final double delta) {
+        System.out.println("Testing " + methodName);
         for (final var task : tasks) {
             test(task, delta);
         }
     }
 
     public void test(final MinimizationTask task, final double delta) {
+        System.out.println(task);
         final AnalyticMinimizer minimizer = minimizerConstructor.create(task.function, task.startX, task.eps);
         final Vector actualAns = minimizer.findMinimum().orElseThrow();
+        if (showIterationsNumber) {
+            final var points = minimizer.points().collect(Collectors.toList());
+            System.out.println("Actual ans: " + points.get(points.size() - 1));
+            System.out.println("Iterations: " + (points.size() - 1));
+            if (showPoints) {
+                System.out.println("Points:");
+                points.forEach(System.out::println);
+            }
+        }
         Assert.assertEquals(0, actualAns.subtract(task.expectedAns).getEuclideanNorm(), delta);
+        System.out.println("OK\n");
     }
 
     public static class MinimizationTask {
@@ -71,6 +98,15 @@ public class MinimizationTester {
 
         public MinimizationTask(final AnalyticFunction function, final Vector ans, final Vector startX) {
             this(function, ans, startX, STANDARD_EPS);
+        }
+
+        @Override
+        public String toString() {
+            return "------Minimization-Task--------\n" +
+                    "f(x) = " + function + "\n" +
+                    "x0 = " + startX + "\n" +
+                    "eps = " + eps + "\n" +
+                    "expected ans = " + expectedAns;
         }
     }
 }
--- test/methods/multidimensional/newton/tasks/Task12.java
+++ test/methods/multidimensional/newton/tasks/Task12.java
@@ -1,8 +1,6 @@
 package methods.multidimensional.newton.tasks;
 
-import methods.multidimensional.newton.methods.AnalyticMinimizer;
-import methods.multidimensional.newton.methods.FastestDescentV2;
-import methods.multidimensional.newton.methods.MinimizationTester;
+import methods.multidimensional.newton.methods.*;
 import models.Vector;
 import models.functions.AnalyticFunction;
 import org.junit.Test;
@@ -19,11 +17,34 @@ public class Task12 {
             new Vector(-1.2, 1)
     );
 
-    @Test
-    public void testFastestDescent() {
-        final var tester = new MinimizationTester(FastestDescentV2::new, 1e-8);
+    private void test(final AnalyticMinimizer.AnalyticMinimizerCons cons, final double eps, final double delta, final String methodName) {
+        final var tester = new MinimizationTester(cons, eps, true, false, methodName);
         tester.addTask(f1);
         tester.addTask(f2);
-        tester.testAll(1e-6);
+        tester.testAll(delta);
+    }
+
+    private void testNewton(final AnalyticMinimizer.AnalyticMinimizerCons cons, final double eps, final String methodName) {
+        test(cons, eps, eps, methodName);
+    }
+
+    @Test
+    public void testFastestDescent() {
+        test(FastestDescentV2::new, 1e-7, 1e-6, "Наискорейший спуск");
+    }
+
+    @Test
+    public void testClassicNewton() {
+        testNewton(NewtonMethod::new, 1e-6, "Метод Ньютона");
+    }
+
+    @Test
+    public void testNewtonWithFind() {
+        testNewton(NewtonWithFind::new, 1e-6, "Ньютон с одномерной минимизацией");
+    }
+
+    @Test
+    public void testNewtonWithDD() {
+        testNewton(NewtonWithDD::new, 1e-6, "Ньютон с выбором направления спуска");
     }
 }
